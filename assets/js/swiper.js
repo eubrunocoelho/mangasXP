851fa37f0360bb928747c790ddade5d0132784ed
Bruno Coelho <eu.brunocoelho94@gmail.com>
2023-06-02 17:59:50 -0300

    update swiper.js

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -1,12 +1,1 @@
-var swiper = new Swiper('.swiper', {
-    slidesPerView: 'auto',
-    spaceBetween: 20,
-    breakpoints: {
-        320: {
-            spaceBetween: 14
-        },
-        768: {
-            spaceBetween: 16
-        }
-    }
-});
+var swiper = new Swiper('.swiper', { slidesPerView: 'auto', spaceBetween: 20, breakpoints: { 320: { spaceBetween: 14 }, 768: { spaceBetween: 16 } } });
