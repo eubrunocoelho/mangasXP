3a250728e8d224cf5da1888a02694bbf7458b17f
Bruno Coelho <eu.brunocoelho94@gmail.com>
2023-06-14 15:12:15 -0300

    finished side navigation script

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,6 +4,28 @@ const
 const
     windowOverlay = $('.window-overlay'),
     sideNavigation = $('.side-navigation'),
-    openSideNavigation = $('#openSideNavigation'),
-    closeSideNavigation = $('#closeSideNavigation');
+    openSideNavigationBtn = $('#openSideNavigation'),
+    closeSideNavigationBtn = $('#closeSideNavigation');
+
+openSideNavigationBtn.addEventListener('click', () => {
+    openSideNavigation();
+
+    closeSideNavigationBtn.addEventListener('click', () => closeSideNavigation());
+
+    window.addEventListener('click', (e) => {
+        if (e.target == windowOverlay) closeSideNavigation();
+    });
+});
+
+let openSideNavigation = () => {
+    windowOverlay.style.display = 'block';
+
+    setTimeout(() => sideNavigation.style.left = '0', 200);
+};
+
+let closeSideNavigation = () => {
+    sideNavigation.style.left = '-290px';
+
+    setTimeout(() => windowOverlay.style.display = 'none', 200);
+};
 
