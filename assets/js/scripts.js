181264f2b1347a650543fa8a90abd5b6692d264f
Bruno Coelho <eu.brunocoelho94@gmail.com>
2023-06-14 15:49:08 -0300

    script to show more genres finished

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,21 +1,26 @@
-const
-    $ = document.querySelector.bind(document);
+/**
+ * =================================================================================
+ * SIDE NAVIGATION
+ * =================================================================================
+ */
 
 const
-    windowOverlay = $('.window-overlay'),
-    sideNavigation = $('.side-navigation'),
-    openSideNavigationBtn = $('#openSideNavigation'),
-    closeSideNavigationBtn = $('#closeSideNavigation');
+    windowOverlay = document.querySelector('.window-overlay'),
+    sideNavigation = document.querySelector('.side-navigation'),
+    openSideNavigationBtn = document.querySelector('#openSideNavigation'),
+    closeSideNavigationBtn = document.querySelector('#closeSideNavigation');
 
-openSideNavigationBtn.addEventListener('click', () => {
-    openSideNavigation();
+if (windowOverlay && sideNavigation && openSideNavigationBtn && closeSideNavigationBtn) {
+    openSideNavigationBtn.addEventListener('click', () => {
+        openSideNavigation();
 
-    closeSideNavigationBtn.addEventListener('click', () => closeSideNavigation());
+        closeSideNavigationBtn.addEventListener('click', () => closeSideNavigation());
 
-    window.addEventListener('click', (e) => {
-        if (e.target == windowOverlay) closeSideNavigation();
+        window.addEventListener('click', (e) => {
+            if (e.target == windowOverlay) closeSideNavigation();
+        });
     });
-});
+}
 
 let openSideNavigation = () => {
     windowOverlay.style.display = 'block';
@@ -29,3 +34,34 @@ let closeSideNavigation = () => {
     setTimeout(() => windowOverlay.style.display = 'none', 200);
 };
 
+/**
+ * =================================================================================
+ * ASIDE GENRES (SHOW MORE)
+ * =================================================================================
+ */
+
+const
+    genresItems = document.querySelectorAll('.aside-genres__list-item'),
+    showMoreGenresBtn = document.querySelector('#showMoreGenres');
+
+if (genresItems && showMoreGenresBtn) {
+    let displayItems = 6;
+
+    for (let i = 0; i < displayItems; i++) {
+        genresItems[i].style.display = 'block';
+    }
+
+    showMoreGenresBtn.addEventListener('click', () => {
+        for (let i = 0; i < genresItems.length; i++) {
+            if (genresItems[i].style.display != 'block') genresItems[i].style.display = 'block';
+        }
+
+        let count = 0;
+
+        for (let i = 0; i < genresItems.length; i++) {
+            if (genresItems[i].style.display != 'none') count++;
+        }
+
+        if (count == genresItems.length) showMoreGenresBtn.style.display = 'none';
+    });
+}
