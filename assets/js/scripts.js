f894304551ef58ad1ee50ec77ddad300b64484fb
Bruno Coelho <eu.brunocoelho94@gmail.com>
2023-06-14 14:52:45 -0300

    modified styles.css and created scripts.js

---
+++ assets/js/scripts.js
@@ -0,0 +1,9 @@
+const
+    $ = document.querySelector.bind(document);
+
+const
+    windowOverlay = $('.window-overlay'),
+    sideNavigation = $('.side-navigation'),
+    openSideNavigation = $('#openSideNavigation'),
+    closeSideNavigation = $('#closeSideNavigation');
+
